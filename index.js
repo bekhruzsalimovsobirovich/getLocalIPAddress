29b38fc790dbcfb732dde8aad8c4b0f02e889049
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 <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>
2024-01-30 15:54:56 +0500

    get local ip address in pc

---
+++ index.js
@@ -0,0 +1,33 @@
+const os = require('os');
+
+const express = require('express')
+const app = express()
+const port = 3030
+
+// Get network interfaces
+const networkInterfaces = os.networkInterfaces();
+
+// Extract local IPv4 addresses
+const localAddresses = [];
+Object.keys(networkInterfaces).forEach(interfaceName => {
+    const interfaces = networkInterfaces[interfaceName];
+    interfaces.forEach(interfaceInfo => {
+        if (interfaceInfo.family === 'IPv4' && !interfaceInfo.internal) {
+            localAddresses.push(interfaceInfo.address);
+        }
+    });
+});
+
+app.get('/', (req, res) => {
+    // Sending a JSON response
+    const jsonData = { ip: localAddresses };
+    res.json(jsonData);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+// app.get('/', (req, res) => res.send(localAddresses))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
