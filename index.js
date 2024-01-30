ba52812d4488251c1cdb05979c3f28d7f606f30a
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 <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>
2024-01-30 16:48:50 +0500

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const os = require('os');
 
 const express = require('express')
 const app = express()
-const port = 3030
+const port = 3000
 
 // Get network interfaces
 const networkInterfaces = os.networkInterfaces();
